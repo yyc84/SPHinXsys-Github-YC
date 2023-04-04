b997aa9ab4fafe1e557823140bba1067c260795d
yongchuan.yu <yongchuan.yu@tum.de>
2023-04-04 17:32:07 +0200

    add case for human body, not works

---
+++ tests/user_examples/ycy_human_body/human_body.cpp
@@ -0,0 +1,118 @@
+/**
+ * @file 	particle_relaxation_single_resolution.cpp
+ * @brief 	This is the test of using levelset to generate particles with single resolution and relax particles.
+ * @details We use this case to test the particle generation and relaxation for a complex geometry.
+ *			Before particle generation, we clean the sharp corners of the model.
+ * @author 	Yongchuan Yu and Xiangyu Hu
+ */
+
+#include "sphinxsys.h"
+using namespace SPH;
+//----------------------------------------------------------------------
+//	Setting for the first geometry.
+//	To use this, please commenting the setting for the second geometry.
+//----------------------------------------------------------------------
+//std::string full_path_to_file = "./input/SPHinXsys.stl";
+//----------------------------------------------------------------------
+//	Basic geometry parameters and numerical setup.
+//----------------------------------------------------------------------
+/*Vec3d domain_lower_bound(-2.3, -0.1, -0.3);
+Vec3d domain_upper_bound(2.3, 4.5, 0.3);
+Vecd translation(0.0, 0.0, 0.0);
+Real scaling = 1.0; */
+//----------------------------------------------------------------------
+//	Setting for the second geometry.
+//	To use this, please commenting the setting for the first geometry.
+//----------------------------------------------------------------------
+//std::string full_path_to_file = "./input/simple-body.stl";
+//Vec3d domain_lower_bound(-270, -95, -740);
+//Vec3d domain_upper_bound(270, 122, 951);
+//Vecd translation(0.0, 0.0, 0.0);
+//Real scaling = 1.0; 
+
+std::string full_path_to_file = "./input/blood-vessel.stl";
+Vec3d domain_lower_bound(-91, -4, -23);
+Vec3d domain_upper_bound(91, 461, 11);
+Vecd translation(0.0, 0.0, 0.0);
+Real scaling = 1.0;
+//----------------------------------------------------------------------
+//	Basic geometry parameters and numerical setup.
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+//	Below are common parts for the two test geometries.
+//----------------------------------------------------------------------
+BoundingBox system_domain_bounds(domain_lower_bound, domain_upper_bound);
+Real dp_0 = (domain_upper_bound[0] - domain_lower_bound[0]) / 50.0;
+//----------------------------------------------------------------------
+//	define the imported model.
+//----------------------------------------------------------------------
+class SolidBodyFromMesh : public ComplexShape
+{
+public:
+	explicit SolidBodyFromMesh(const std::string &shape_name) : ComplexShape(shape_name)
+	{
+		add<TriangleMeshShapeSTL>(full_path_to_file, translation, scaling);
+	}
+};
+//-----------------------------------------------------------------------------------------------------------
+//	Main program starts here.
+//-----------------------------------------------------------------------------------------------------------
+int main()
+{
+	//----------------------------------------------------------------------
+	//	Build up -- a SPHSystem
+	//----------------------------------------------------------------------
+	SPHSystem system(system_domain_bounds, dp_0);
+	IOEnvironment io_environment(system);
+	//----------------------------------------------------------------------
+	//	Creating body, materials and particles.
+	//----------------------------------------------------------------------
+	RealBody imported_model(system, makeShared<SolidBodyFromMesh>("SolidBodyFromMesh"));
+	// level set shape is used for particle relaxation
+	imported_model.defineBodyLevelSetShape()->correctLevelSetSign()->writeLevelSet(io_environment);
+	imported_model.defineParticlesAndMaterial();
+	imported_model.generateParticles<ParticleGeneratorLattice>();
+	//----------------------------------------------------------------------
+	//	Define simple file input and outputs functions.
+	//----------------------------------------------------------------------
+	BodyStatesRecordingToVtp write_imported_model_to_vtp(io_environment, {imported_model});
+	MeshRecordingToPlt write_cell_linked_list(io_environment, imported_model.getCellLinkedList());
+	//----------------------------------------------------------------------
+	//	Define body relation map.
+	//	The contact map gives the topological connections between the bodies.
+	//	Basically the the range of bodies to build neighbor particle lists.
+	//----------------------------------------------------------------------
+	InnerRelation imported_model_inner(imported_model);
+	//----------------------------------------------------------------------
+	//	Methods used for particle relaxation.
+	//----------------------------------------------------------------------
+	SimpleDynamics<RandomizeParticlePosition> random_imported_model_particles(imported_model);
+	/** A  Physics relaxation step. */
+	relax_dynamics::RelaxationStepInner relaxation_step_inner(imported_model_inner, true);
+	//----------------------------------------------------------------------
+	//	Particle relaxation starts here.
+	//----------------------------------------------------------------------
+	random_imported_model_particles.exec(0.25);
+	relaxation_step_inner.SurfaceBounding().exec();
+	write_imported_model_to_vtp.writeToFile(0.0);
+	imported_model.updateCellLinkedList();
+	write_cell_linked_list.writeToFile(0.0);
+	//----------------------------------------------------------------------
+	//	Particle relaxation time stepping start here.
+	//----------------------------------------------------------------------
+	int ite_p = 0;
+	while (ite_p < 1000)
+	{
+		relaxation_step_inner.exec();
+		ite_p += 1;
+		if (ite_p % 100 == 0)
+		{
+			std::cout << std::fixed << std::setprecision(9) << "Relaxation steps for the imported model N = " << ite_p << "\n";
+			write_imported_model_to_vtp.writeToFile(ite_p);
+		}
+	}
+	std::cout << "The physics relaxation process of imported model finish !" << std::endl;
+
+	return 0;
+}
