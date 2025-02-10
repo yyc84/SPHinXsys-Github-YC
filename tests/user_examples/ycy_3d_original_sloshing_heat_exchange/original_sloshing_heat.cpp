2173074eb118b9d89431b06c93bd7fe28593d9e0
Yongchuan Yu <yongchuan.yu@tum.de>
2025-02-10 20:13:47 +0100

    update the new geometries

--- tests/user_examples/ycy_3d_original_sloshing_heat_exchange/original_sloshing_heat.cpp
+++ tests/user_examples/ycy_3d_original_sloshing_heat_exchange/original_sloshing_heat.cpp
@@ -18,11 +18,11 @@ int main(int ac, char* av[])
 	/* Build up -- a SPHSystem -- */
 	SPHSystem system(system_domain_bounds, resolution_ref);
 	// Tag for run particle relaxation for the initial body fitted distribution.
-	system.setRunParticleRelaxation(false);
+	system.setRunParticleRelaxation(true);
 	// Tag for computation start with relaxed body fitted particles distribution.
-	system.setReloadParticles(true);
+	system.setReloadParticles(false);
 	/* Tag for computation from restart files. 0: start with initial condition. */
-	system.setRestartStep(0);
+	system.setRestartStep(1);
 	//handle command line arguments
 	system.handleCommandlineOptions(ac, av);
 	/* Output environment. */
@@ -33,7 +33,7 @@ int main(int ac, char* av[])
 	@Brief creating body, materials and particles for the cylinder.
 	*/
 	SolidBody tank(system, makeShared<Tank>("Tank"));
-	//tank.defineBodyLevelSetShape()->writeLevelSet(system);
+	tank.defineBodyLevelSetShape()->writeLevelSet(system);
 	tank.defineMaterial<Solid>();
 	(!system.RunParticleRelaxation() && system.ReloadParticles())
 		? tank.generateParticles<BaseParticles, Reload>(tank.getName())
@@ -283,7 +283,7 @@ int main(int ac, char* av[])
 	size_t number_of_iterations = system.RestartStep();
 	int screen_output_interval = 100;
 	int restart_output_interval = screen_output_interval * 10;
-	Real End_Time = 5.0;			/**< End time. */
+	Real End_Time = 21.5;			/**< End time. */
 	Real D_Time = 0.1;	/**< time stamps for output. */
 	Real dt = 0.0; 					/**< Default acoustic time step sizes for fluid. */
 
@@ -345,7 +345,7 @@ int main(int ac, char* av[])
                 air_density_relaxation.exec(dt);
 
 				/*Thermal relaxation*/
-                if (GlobalStaticVariables::physical_time_>= 0.5)
+                if (GlobalStaticVariables::physical_time_>= 1.5)
                 {
                     water_heat_exchange_complex.exec(dt);
                     air_heat_exchange_complex.exec(dt);
@@ -417,7 +417,7 @@ int main(int ac, char* av[])
             water_air_complex.updateConfiguration();
             air_water_complex.updateConfiguration();
 
-			if (GlobalStaticVariables::physical_time_ >= 0.5)
+			if (GlobalStaticVariables::physical_time_ >= 1.5)
 			{
                 wave_probe_S1.writeToFile();
                 wave_probe_S2.writeToFile();
--- tests/user_examples/ycy_3d_original_sloshing_heat_exchange/tank_case_heat.h
+++ tests/user_examples/ycy_3d_original_sloshing_heat_exchange/tank_case_heat.h
@@ -12,71 +12,75 @@ using namespace SPH;
 /*@brief Basic geometry parameters and numerical setup.
 */
 
+
 Real resolution_ref = 0.008;   /* Initial particle spacing*/
-
-
 /* Domain bounds of the system*/
 BoundingBox system_domain_bounds(Vec3d(-0.2, -0.05, -0.2), Vec3d(0.2, 1.0, 0.2));
 
 
 /*Material properties of the water and air.*/
-//Real rho0_f = 1000.0;         /*Fluid density*/
-//Real rho0_a = 1.226;            /*Air density*/
-//Real gravity_g = 9.81;        /*Gravity force of fluid*/
-//Real U_f = 2.0 * sqrt(gravity_g * 0.5);	/**< Characteristic velocity. */
-//Real U_g = 2.0 * sqrt(gravity_g * 0.5);  	/**< dispersion velocity in shallow water. */
-//Real c_f = 10.0 * SMAX(U_g, U_f);	/**< Reference sound speed. */
-//Real f = 1.7;
-//Real a = 0.04;
-//Real c_p_water = 4.179e3;
-//Real c_p_air = 1.012e3;
-//Real k_water = 0.620;
-//Real k_air = 0.0254;
-//Real diffusion_coff_water = k_water / (c_p_water * rho0_f);
-//Real diffusion_coff_air = k_air / (c_p_air * rho0_a);
-//Real mu_water = 653.9e-6;
-//Real mu_air = 20.88e-6;
-//Real length_scale = 1.0;
-//Vec3d translation(0, 0.0, 0);
-//Real initial_temperature_right = 313.15;
-//Real initial_temperature_air = 353.15;
-
-/*parameters for Methane and Nitrogen*/
-Real rho0_f = 424.7;                   /*Fluid density*/
-Real rho0_a = 2.668;                    /*Air density*/
-Real gravity_g = 9.81;                  /*Gravity force of fluid*/
-Real U_f = 2.0 * sqrt(gravity_g * 0.5); /**< Characteristic velocity. */
-Real U_g = 2.0 * sqrt(gravity_g * 0.5); /**< dispersion velocity in shallow water. */
-Real c_f = 10.0 * SMAX(U_g, U_f);       /**< Reference sound speed. */
+Real rho0_f = 1000.0;         /*Fluid density*/
+Real rho0_a = 1.226;            /*Air density*/
+Real gravity_g = 9.81;        /*Gravity force of fluid*/
+Real U_f = 2.0 * sqrt(gravity_g * 0.5);	/**< Characteristic velocity. */
+Real U_g = 2.0 * sqrt(gravity_g * 0.5);  	/**< dispersion velocity in shallow water. */
+Real c_f = 10.0 * SMAX(U_g, U_f);	/**< Reference sound speed. */
 Real f = 1.7;
 Real a = 0.04;
-Real c_p_water = 3.4267e3;
-Real c_p_air = 1.054e3;
-Real k_water = 0.1846;
-Real k_air = 0.01221;
+Real c_p_water = 4.179e3;
+Real c_p_air = 1.012e3;
+Real k_water = 0.620;
+Real k_air = 0.0254;
 Real diffusion_coff_water = k_water / (c_p_water * rho0_f);
 Real diffusion_coff_air = k_air / (c_p_air * rho0_a);
-Real mu_water = 121.79e-6;
-Real mu_air = 8.73e-6;
+Real mu_water = 653.9e-6;
+Real mu_air = 20.88e-6;
 Real length_scale = 1.0;
 Vec3d translation(0, 0.0, 0);
-Real initial_temperature_right = 110.15;
-Real initial_temperature_air = 130.15;
+Real initial_temperature_right = 313.15;
+Real initial_temperature_air = 353.15;
+
+/*parameters for Methane and Nitrogen*/
+//Real rho0_f = 424.7;                   /*Fluid density*/
+//Real rho0_a = 2.668;                    /*Air density*/
+//Real gravity_g = 9.81;                  /*Gravity force of fluid*/
+//Real U_f = 2.0 * sqrt(gravity_g * 0.5); /**< Characteristic velocity. */
+//Real U_g = 2.0 * sqrt(gravity_g * 0.5); /**< dispersion velocity in shallow water. */
+//Real c_f = 10.0 * SMAX(U_g, U_f);       /**< Reference sound speed. */
+//Real f = 1.7;
+//Real a = 0.03;
+//Real c_p_water = 3.4267e3;
+//Real c_p_air = 1.054e3;
+//Real k_water = 0.1846;
+//Real k_air = 0.01221;
+//Real diffusion_coff_water = k_water / (c_p_water * rho0_f);
+//Real diffusion_coff_air = k_air / (c_p_air * rho0_a);
+//Real mu_water = 121.79e-6;
+//Real mu_air = 8.73e-6;
+//Real length_scale = 1.0;
+//Vec3d translation(0, 0.0, 0);
+//Real initial_temperature_right = 110.15;
+//Real initial_temperature_air = 130.15;
 
 
-std::string fuel_tank_outer = "./input/tank_outer.STL";
-std::string fuel_tank_inner = "./input/tank_inner.STL";
-std::string water_05 = "./input/water_05.STL";
-std::string air_05 = "./input/gas_05.STL";
-std::string probe_s1_shape = "./input/ProbeS1.STL";
-std::string probe_s2_shape = "./input/ProbeS2.STL";
-std::string probe_s3_shape = "./input/ProbeS3.STL";
+std::string fuel_tank_outer = "./input/tank_outer_small.stl";
+std::string air_05 = "./input/gas_small.stl";
+std::string probe_s1_shape = "./input/ProbeS1.stl";
+std::string probe_s2_shape = "./input/ProbeS2.stl";
+std::string probe_s3_shape = "./input/ProbeS3.stl";
+//std::string fuel_tank_inner = "./input/tank_inner_small.stl";
+//std::string water_05 = "./input/water_small.stl";
 
-//std::string water_05 = "./input/water_2_hole.STL";
-//std::string fuel_tank_inner = "./input/tank_inner_2_hole.STL";
-    /*
-Fuel Tank.
-*/
+//std::string fuel_tank_inner = "./input/tank_inner_2hole_small.stl";
+//std::string water_05 = "./input/water_2hole_small.stl";
+
+//std::string fuel_tank_inner = "./input/tank_inner_9hole_small.stl";
+//std::string water_05 = "./input/water_9hole_small.stl";
+
+std::string fuel_tank_inner = "./input/tank_inner_ring_small.stl";
+std::string water_05 = "./input/water_ring_small.stl";
+
+/*Fuel Tank.*/
 class Tank : public ComplexShape
 {
 public:
@@ -115,7 +119,7 @@ class VariableGravity : public Gravity
     virtual Vecd InducedAcceleration(const Vecd &position = Vecd::Zero()) override
     {
         time_ = GlobalStaticVariables::physical_time_;
-        if (time_ <= 0.5)
+        if (time_ <= 1.5)
         {
             global_acceleration_ = global_acceleration_;
         }
